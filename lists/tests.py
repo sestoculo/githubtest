6f7f8f460465c9431c3168a4738aab3df17bdb3e
Sesto Culo <tonci@protonmail.com>
2018-03-10 18:57:14 +0100

    A new setup for model

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 # Create your tests here.
+# Adding for testing model and database ORM (Object-Relation Maper)
+from lists.models import item
 
 class HomePageTest (TestCase):
     
@@ -30,12 +32,26 @@ class HomePageTest (TestCase):
         )
         #expected_html= render_to_string('home.html')
         self.assertEqual(response.content.decode(),expected_html)
+
+class ItemModelTest (TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item = 'The first (ever) list item'
+        first_item.save()
         
+        second_item = Item()
+        second_item = 'The the second'
+        second_item.save()
+
+        saved_items = Item.Object.All()
+        self.assertEqual (saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual (first_saved_item.text, 'The first (ever) list item' )
+        self.assertEqual (second_saved_item.text, 'Item the second' )
 
 
 
-'''         self.assertTrue(response.content.strip.startswith(b'<html>'))
-        self.assertIn(b'<title>To-DO lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'/html')) '''
 
 
