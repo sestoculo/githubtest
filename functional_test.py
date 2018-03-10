1aa0a6827f4c8e11a0c76d8bc05950e897b4048b
Sesto Culo <tonci@protonmail.com>
2018-03-10 10:59:51 +0100

    Version before refactoring ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- functional_test.py
+++ functional_test.py
@@ -33,12 +33,10 @@ class NewVisitorTest (unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy pacock feather' for row in rows),
-            "New to-do item did not apper in tablev -- its text was:\n%s" % (
-                table.text,
-            )
-        )
+        self.assertIn('1: Buy peacock feather', [row.text for row in rows])
+        self.assertIn('2: Use peacock feather to make a fly',[row.text for row in rows])
+        
+        
 
         #There is a still a text box inviting her to add another item. She enters
         #"Use peacock feathers to make a fly" (Edith is very methodical)
